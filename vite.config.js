3fc293d2c1f336ce6975d29ea9cb749006bf702e
Akshit Kamboj <akshitkamboz13@gmail.com>
2025-04-23 10:57:55 +0530

    update v2.1.3 remove gh-pages configuration

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio.io/',
-
+  
   // Configure build options
   build: {
     outDir: 'dist', // default build directory for Vite
